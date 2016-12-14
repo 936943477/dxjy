124c4ce239f2c097cf4cc398a0d014c8b1b90f22
搞机哥 <weelvis@qq.com>
2016-12-14 13:42:13 +0800

    Merge remote-tracking branch '936943477/master'
    # Conflicts:
    #	.idea/workspace.xml

--- js/Package.js
+++ js/Package.js
@@ -1,29 +1,25 @@
 $("header").load("hearder.html");
 $("footer").load("footer.html");
 //****************************$.get();
-function get(){
-    var www=window.location.href;
-    www=www.slice(-2,www.length);
-    var app=angular.module('myApp',[]);
-    app.controller('siteCtrl',function ($scope, $http) {
-        function getjson(url){
-            $http.get(url,{params:{id:www}}).success(function (response) {
-                $scope.cp=response;
-            });
-        }
-        getjson("data/listOne.php");
-    });//anglar.js 调用php
-    //jq  click 传送买卖数据
-    $("#buy").click(function (event) {
-        event.preventDefault();
-        http("paytwo.php",$("#buyUnitPrice").val(),$("#buyset").val(),www,"buy","111");
-    });
-    $("#sell").click(function (event) {
-        event.preventDefault();
-        http("data/doBuySell.php",$("#sellUnitPrice").val(),$("#sellset").val(),www,"sell","111");
-    });
+var T="";
+var www=window.location.href;
+www=www.slice(-2,www.length);
+var app=angular.module('myApp',[]);
+app.controller('siteCtrl',function ($scope, $http) {
+    function getjson(url){
+        $http.get(url,{params:{id:www}}).success(function (response) {
+            $scope.cp=response;
+            T=response;
+            console.log(T);
+            get(T);
+        });
+    }
+    getjson("data/listOne.php");
+});//anglar.js 调用php
+//*********************************  form
+function get(T){
+    var form=$("form");
+    form[0].action+="?id="+www+"&chanpin="+T.chanpin+"&buybzj="+T.buybzj+"&baozhuang="+T.baozhuang;
+    form[1].action+="?id="+www+"&chanpin="+T.chanpin+"&buybzj="+T.buybzj+"&baozhuang="+T.baozhuang;
 }
-get();
-function http(url,UnitPrices,sets,ids,buySells,uesrs){
-    $.get(url,{UnitPrice:UnitPrices,set:sets,id:ids,buySell:buySells,uesr:uesrs});
-}
+
